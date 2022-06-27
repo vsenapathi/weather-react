dbb5459bb36232955eab53b29133be6cf887458b
vsenapathi <vathsalya.senapathi@gmail.com>
2022-06-27 16:05:57 -0500

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      Weather App
+      <footer>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/vsenapathi/weather-react"
           target="_blank"
-          rel="noopener noreferrer"
+          title="Github Repository "
         >
-          Learn React
-        </a>
-      </header>
+          Open Source Code
+        </a>{" "}
+        by Vathsalya Senapathi
+      </footer>
     </div>
   );
 }
